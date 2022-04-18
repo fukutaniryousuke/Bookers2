3a6e41d32d025252728c00ebf02f09bd791a2dc2
EC2 Default User <ec2-user@ip-172-31-22-121.ec2.internal>
2022-04-18 11:31:04 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  belongs_to :user
+ validates :title, presence: true
+ validates :body, presence: true, length: { maximum: 200 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  root to: "homes#top"
-  devise_for :books
+  
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update, :index]
+  
+  root to: "homes#top"
+  get '/homes/about' => "homes#about", as: "about"
 end
