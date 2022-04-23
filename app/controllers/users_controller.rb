bbe50861857268f363d68e1e7d8350090b5b1093
EC2 Default User <ec2-user@ip-172-31-22-121.ec2.internal>
2022-04-23 07:51:15 +0000

    [Add]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,8 +35,8 @@ class UsersController < ApplicationController
 
 private
 
-def user_params
+ def user_params
   params.require(:user).permit(:name, :introduction, :profile_image)
-end
+ end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+ 
+  resource :favorites, only: [:create, :destroy]
+end
   
+  resources :users, only: [:show, :edit, :update, :index]
   root to: "homes#top"
 end
