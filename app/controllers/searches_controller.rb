fe1a6b3172c8ddbe7104f70e102e7c9cfcf4fbf6
EC2 Default User <ec2-user@ip-172-31-22-121.ec2.internal>
2022-04-25 11:01:54 +0000

    [Add]

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,36 @@
+class SearchesController < ApplicationController
+
+
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+
+  def search_for(model, content, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == "forcontent"
+        User.where('name LIKE ?', "#{content}%")
+      elsif method == "backcontent"
+        User.where('name LIKE ?', "%#{content}")
+      else 
+        User.where('name LIKE ?', "%#{content}%")
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(title: content)
+      elsif method == "forcontent"
+        Book.where('title LIKE ?', "#{content}%")
+      elsif method == "backcontent"
+        Book.where('title LIKE ?', "%#{content}")
+      else
+        Book.where('title LIKE ?', "%#{content}%")
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'relationships/followings'
   get 'relationships/followers'
   get "/home/about" => "homes#about", as: "about"
-  
+  get '/search', to: 'searches#search'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
